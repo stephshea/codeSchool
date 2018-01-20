06a81264e5b95764117be1053a44a2fa0cbaf10a
Stephanie Shea <stephcshea@gmail.com>
2018-01-20 23:20:54 +0000

    second commit before branching

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(request, response) {
+    response.send('Hello world');
+});
+app.get('/name', function(request, response) {
+   var name = 'Stephanie';
+   response.send(name);
+});
+
+app.get('/surprise', function(request, response) {
+    response.send('Surprise!');
+});
+
+app.get('/redirect', function(request, response) {
+    response.redirect(301, '/surprise');
+});
+
+app.get('/date', function(request, response) {
+   var date = new Date();
+   response.send('It is ' +date);
+});
+
+app.listen(process.env.PORT);
+
---
+++ date.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+// middleware that is specific to this router
+router.use(function timeLog(req, res, next) {
+  console.log('Time: ', Date.now());
+  next();
+});
+
+module.exports = router;
