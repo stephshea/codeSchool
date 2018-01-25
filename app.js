a845c9df21f13d848bc7fd05d011a59b8d1a8a8e
Stephanie Shea <stephcshea@gmail.com>
2018-01-25 21:27:45 +0000

    intial commit level 4

--- app.js
+++ app.js
@@ -1,41 +1,88 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+var parseUrlencoded = bodyParser.urlencoded({ extended: false});
+
+var cities = {
+	'Providence': 'RI',
+	'Braintree': 'MA',
+	'Putnam': 'CT',
+	'Dallas': 'TX',
+	'Jacksonville': 'FL',
+	'Boise': 'ID',
+	'Chicago': 'IL',
+	'test': 'Test'
+};
+
+app.post('/cities', parseUrlencoded, function(request, response) {
+    if(request.body.newCity.length >=4 && request.body.newCity.state.length >=2){
+    var newCity = request.body;
+    
+    cities[newCity.name] = newCity.state;
+    response.status(201).json(newCity.name);
+}
+else {
+    response.status(400).json("City must have at least 4 letters; state 2");
+}
+});
+
+
 app.get('/', function(request, response) {
-    response.send('Hello world');
+	response.send('Hello world');
 });
 app.get('/name', function(request, response) {
-   var name = 'Stephanie';
-   response.send(name);
+	var name = 'Stephanie';
+	response.send(name);
 });
-
 app.get('/surprise', function(request, response) {
-    response.send('Surprise!');
+	response.send('Surprise!');
 });
-
 app.get('/redirect', function(request, response) {
-    response.redirect(301, '/surprise');
+	response.redirect(301, '/surprise');
 });
-
 app.get('/date', function(request, response) {
-   var date = new Date();
-   response.send('It is ' +date);
+	var date = new Date();
+	response.send('It is ' + date);
 });
-
-app.get('/cities', function(request, response) {
-    var cities = ['Providence', 'Boston', 'Dallas', 'Jacksonville', 'Boise'];
-    
-    
-    response.json(cities);
-});
-
-// app.get('/cities', function(request, response) {
+app.use(express.static('public'));
+// instead of static can use app.get('/cities', function(request, response) {
 //     response.sendFile(__dirname + '/public/index.html')
 // });
+app.get('/cities', function(request, response) {
+	//  var cities = ['Providence', 'Boston', 'Dallas', 'Jacksonville', 'Boise'];
+	if (request.query.limit >= 0) {
+		response.json(cities.slice(0, request.query.limit));
+	} else if (request.query.limit > cities.length) {
+		response.status(404).json('your query was too large' + request.params.name);
+		console.log("error");
+	} else {
+		response.json(cities);
+	}
+});
 
-app.use(express.static('public'));
 
 
-app.use(express.static('public'));
+app.param('name', function(request, response, next) {
+	var name = request.params.name;
+	var city = name[0].toUpperCase() + name.slice(1).toLowerCase();
+	request.cityName = city;
+	next();
+});
 
+app.get('/cities/:name', function(request, response) {
+	// var name = request.params.name;
+	// var city = name[0].toUpperCase() + name.slice(1).toLowerCase();
+	var state = cities[request.cityName];
+	if (!state) {
+		response.status(404).json("No city found" + request.params.name);
+	} else {
+		response.json(state);
+	}
+	console.log("hi");
+});
+// function parseCityName(name) {
+//     var parsedName = name[0].toUpperCase() + name.slice(1).toLowerCase();
+//     return parsedName;
+//     }
 app.listen(process.env.PORT);
--- public/client.js
+++ public/client.js
@@ -1,16 +1,34 @@
 /* global $ */
 $(document).ready(function() {
-$(function(){
-   
-   $.get('/cities', appendToOptions);
+       $.get('/cities', appendToOptions);
    
    function appendToOptions(cities) {
        var options = [];
+       var content, city;
        for( var i in cities) {
+           city = cities[i];
+           content = '<a href="/cities/' +city+'">'+city+'</a> '+'<a href="#" data-city="'+city+'"><img src="del.jpg"></a>';
            options.push($('<option>', { text: cities[i] }));
        }
        $('#cityOps').append(options);
    }
+});
 
+    
+$(function(){
+    $('form').on('submit', function(event) {
+       event.preventDefault();
+       var form =$(this);
+       var cityData = form.serialize();
+       
+       $.ajax({
+           type: 'POST', url: '/cities', data: cityData
+           
+        }).done(function(cityName) {
+           appendToOptions([cityName]);
+           form.trigger('reset');
 });
 });
+});
+   
+   
